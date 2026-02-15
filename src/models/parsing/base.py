f5fccbd4227d31d780d93204aa0b59e4fc30b149
Hariton <pitsikhariton@gmail.com>
2026-02-15 11:53:47 +0400

    fix(core): removed useless duplicate

--- src/models/parsing/base.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class DocumentParser(ABC):
-    """Parsing input file into Document"""
-
-    @abstractmethod
-    def parse_file(self, file_path):
-        pass
